efd9fb1de8f54c0cb4231a129665713ca4fe3807
me <me@me-VirtualBox.(none)>
2014-01-10 13:23:23 -0500

    removed a lot of prints and added onclick to a button

--- bluray/views.py
+++ bluray/views.py
@@ -16,19 +16,19 @@ def track(request):
 	results = {'success':False}
 	if request.method == 'GET':
 		GET = request.GET
-		if GET.has_key('pk') and GET.has_key('vote'):
-			pk = int(GET['pk'])
-			vote = GET['vote']
+		if GET.has_key('id') and GET.has_key('track'):
+			id_in = int(GET['id'])
+			track  = GET['track']
 
-			movie = Movie.objects.get(id=pk)
-			print movie.name
-			if vote == 'up':
-				movie.name = 'jerk'
-			elif vote == 'down':
-				movie.name = 'off'
+			movie = Movie.objects.get(id=id_in)
+			'''
+			if track = 'track' then add user to movie's track list
+			if track == 'untrack' then remove
+			find user through User.objects.get(username = request.user)
+			should probably make sure user is logged in
+			'''
 			movie.save()
 			results = {'success':True}
 
 	jason = json.dumps(results)
-	print 'hi'
 	return HttpResponse(jason, content_type='application/json')
