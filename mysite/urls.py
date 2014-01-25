4054b8eb554342eff06d221b1ef647bc2c7d646e
Leon Lee <93leonlee@gmail.com>
2014-01-25 16:39:16 -0500

    make unique emails a requirement for signing up

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,11 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from registration.forms import RegistrationFormUniqueEmail
+from registration.backends.default.views import RegistrationView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'', include('bluray.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/register/$', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail), name='registration_register'),
     url(r'^accounts/', include('registration.backends.default.urls')),
 )
