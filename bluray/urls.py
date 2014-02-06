d2e2f338e7903720a4e4482e9e6d58efc2dd7280
Leon Lee <93leonlee@gmail.com>
2014-02-05 19:09:56 -0500

    redirect logout to index page

--- bluray/urls.py
+++ bluray/urls.py
@@ -5,5 +5,6 @@ from bluray import views
 urlpatterns = patterns('',
     url(r'^track/$', views.track, name='track'),
     url(r'^loginview/$', views.loginview, name='login'),
+    url(r'logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^$', views.index, name='index'),
 )
