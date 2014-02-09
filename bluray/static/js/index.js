111899cfb5d73329fa3fed845e069a8840a47ae4
Leon Lee <93leonlee@gmail.com>
2014-02-08 21:22:12 -0500

    remove popup for track

--- bluray/static/js/index.js
+++ bluray/static/js/index.js
@@ -1,6 +1,6 @@
 function track(id_in, button) {	
 	$.getJSON("/track/",{id:id_in, track:button.value}, function(result){
-		alert("Successful?: " + result['success']);
+		//alert("Successful?: " + result['success']);
 	});
 	button.value = (button.value == "Track") ? "Untrack" : "Track";
 	$(button).toggleClass('btn-warning');
