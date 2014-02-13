6fc3954f27795d332348543bf1c9894e818687e4
Doug <dougjchau@gmail.com>
2014-02-13 18:07:14 -0500

    implement email function

--- bluray/management/commands/checkdates.py
+++ bluray/management/commands/checkdates.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
 from bluray.models import Movie
-import datetime
 from collections import defaultdict
+from email.mime.text import MIMEText
+import smtplib
+import datetime
 
 '''
 Checks the release dates of every movie in the database by subtracting future release date from today
@@ -29,6 +31,39 @@ class Command(BaseCommand):
 		for movie in release_today:
 			for user in movie.tracking.all():
 				email_list[user.email].append(movie.name)
-		#do something with the emails
 
-		
+		users = email_list.keys()
+		for user in users:
+			sendEmail(user)
+
+def sendEmail(user):
+	sender = "thewafflestand@gmail.com"
+	pwd = "wafflestand1"
+	receiver = user
+	msg = MIMEMultipart('alternative')
+	msg['Subject'] = 'Waffle Stand Alert'
+	msg['From'] = sender
+	msg['To'] = receiver
+	text = "Your movies are out! " + ', '.join(email_list[user])
+	html = """
+		<html>
+			<head></head>
+			<body>
+				<img src="http://interfacelift.com/wallpaper/previews/03454_bonjourleman@2x.jpg">
+				<p>Hi!</p>
+			</body>
+		</html>
+	"""
+	part1 = MIMEText(text, 'plain')
+	part2 = MIMEText(html, 'html')
+	msg.attach(part1)
+	msg.attach(part2)
+	try:
+		server = smtplib.SMTP('smtp.gmail.com', 587)
+		server.ehlo()
+		server.starttls()
+		server.login(sender, pwd)
+		server.sendmail(sender, receiver, msg.as_string())
+		server.quit()
+	except Exception as e:
+		print "Error: " + str(e)
