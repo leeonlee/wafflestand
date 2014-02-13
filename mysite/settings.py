bc341ebb4741a07b823c52dba9409f4da873cb5d
Leon Lee <93leonlee@gmail.com>
2014-02-13 18:34:55 -0500

    test db config

--- mysite/settings.py
+++ mysite/settings.py
@@ -71,12 +71,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -99,7 +94,15 @@ USE_TZ = True
 
 #Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(default='postgres://a:a@localhost/thedb')
+try:
+	DATABASES['default'] = dj_database_url.config(default='postgres://a:a@localhost/thedb')
+except:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		}
+	}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
