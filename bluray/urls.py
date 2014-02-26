97b0c68fdee2251b536f0b7687a542360a083bed
Leon Lee <93leonlee@gmail.com>
2014-02-26 18:30:21 -0500

    move registration and password paths to bluray/urls from mysite/urls

--- bluray/urls.py
+++ bluray/urls.py
@@ -1,10 +1,25 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
+from django.contrib.auth import views as auth_views
 
 from bluray import views
+
+from registration.forms import RegistrationFormUniqueEmail
+from registration.backends.default.views import RegistrationView
+
 
 urlpatterns = patterns('',
     url(r'^track/$', views.track, name='track'),
     url(r'^loginview/$', views.loginview, name='login'),
     url(r'logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+
+    url(r'^accounts/password/change/$', auth_views.password_change, name='password_change'),
+    url(r'^accounts/password/change/done/$', auth_views.password_change_done, name='password_change_done'),
+    url(r'^accounts/password/reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^accounts/password/reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^accounts/password/reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^accounts/register/$', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail), name='registration_register'),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+
     url(r'^$', views.index, name='index'),
 )
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,13 +14,4 @@ urlpatterns = patterns('',
     url(r'', include('bluray.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^password/change/$', auth_views.password_change, name='password_change'),
-    url(r'^password/change/done/$', auth_views.password_change_done, name='password_change_done'),
-    url(r'^password/reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password/reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    url(r'^password/reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
-    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
-
-    url(r'^accounts/register/$', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail), name='registration_register'),
-    url(r'^accounts/', include('registration.backends.default.urls')),
 )
