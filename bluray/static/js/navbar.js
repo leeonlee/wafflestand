42d404f07f0cafc9de9fcd37b9f54a865255acac
Leon Lee <93leonlee@gmail.com>
2014-02-28 17:31:00 -0500

    separated navbar javascript from index.js

---
+++ bluray/static/js/navbar.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	$('#loginmodal').on('shown.bs.modal', function(){
+		$('#id_username').focus();
+	});
+
+	$('#login_form').submit(function() {
+		$.ajax({
+			data: $(this).serialize(),
+			type: $(this).attr('method'),
+			url: "/loginview/",
+			success: function(response){
+				if (response['success'] == 'success'){
+					location.reload();
+				} else if (response['success'] == 'validate'){
+					$('#form-error').html("Check account's email");
+				} else {
+					$('#form-error').html("Invalid credentials");
+				}
+			}
+		});
+		return false
+	});
+	$('#reset_form').submit(function() {
+		$.ajax({
+			data: $(this).serialize(),
+			type: $(this).attr('method'),
+			url: "/accounts/password/reset/",
+			beforeSend: function(xhr){
+				$('#reset-message').html("Please wait..");
+			}
+			
+		}).done(function(response){
+			$('#reset-message').html("Thank you. If an account exists for the provided email, you will receive further instructions to reset your password");
+		});
+		return false
+	});
+});
