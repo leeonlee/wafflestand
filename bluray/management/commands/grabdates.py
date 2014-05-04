74af9452ae87edae7feaf326a6d01b9a69976a00
Leon Lee <93leonlee@gmail.com>
2014-05-04 16:42:34 -0400

    add --now flag to grabdates and grabmovies; change API_KEY to RT_API_KEY

--- bluray/management/commands/grabdates.py
+++ bluray/management/commands/grabdates.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
 from bluray.models import Movie
 from rottentomatoes import RT
 from datetime import datetime
@@ -7,7 +8,7 @@ from django.conf import settings #API KEY and SCRAPE_DAY
 import re
 
 SCRAPE_DAY = getattr(settings, "SCRAPE_DAY", None)
-API_KEY = getattr(settings, "API_KEY", None)
+RT_API_KEY = getattr(settings, "RT_API_KEY", None)
 
 '''
 Scrape the dates of all movies in the database
@@ -15,9 +16,16 @@ Probably want to not scrape for movies already with dates
 '''
 class Command(BaseCommand):
 	help = ''
+	option_list = BaseCommand.option_list + (
+		make_option('--now', 		#Creates the flag '--now'
+		    action='store_true', 	#Stores True inside the specified destination
+		    dest='now', 		#Key destination for value to be saved into
+		    default=False,		
+		    help='Allow command to be run regardless of scrape day'),
+        )
 
 	def handle(self, *args, **options):
-		if datetime.today().isoweekday() != SCRAPE_DAY:
+		if datetime.today().isoweekday() != SCRAPE_DAY and not options['now']:
 			print "NOT SCRAPE DAY!!"
 			return
 		movies = Movie.objects.filter(released=False)
@@ -29,7 +37,7 @@ class Command(BaseCommand):
 				sleep(2)
 
 			try:
-				rt_object = RT(API_KEY).info(movie.rt_id)
+				rt_object = RT(RT_API_KEY).info(movie.rt_id)
 				release_date = rt_object['release_dates'].get('dvd', None)
 				if release_date:
 					# RT release dates look like: 2014-02-13
